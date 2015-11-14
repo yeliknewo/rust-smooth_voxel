996c2be1c76486e8a839b4b68c8a0d3ffb9d992a
Kiley Owen <owenk@wit.edu>
2015-11-14 17:46:14 -0500

    stuff

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,37 @@ mod math;
 
 use glium::{DisplayBuild, Surface, Frame};
 use glium::backend::glutin_backend::GlutinFacade;
+use std::collections::HashMap;
 
-use num::traits::{Float};
+struct Camera{
+    position: [f32; 3],
+    pitch: f32,
+    yaw: f32,
+}
+
+struct Keyboard {
+    keys: HashMap<u8, bool>,
+}
+
+impl Keyboard {
+    pub fn key_down(&self, key_code: u8) -> bool {
+        *self.keys.get(&key_code).unwrap()
+    }
+}
+
+impl Camera {
+    pub fn new(pitch: f32, yaw: f32, position: [f32; 3]) -> Camera{
+        Camera{
+            pitch: pitch,
+            yaw: yaw,
+            position: position,
+        }
+    }
+
+    pub fn view_mat4(&self) -> [[f32; 4]; 4] {
+        math::view_matrix_from_radians(self.pitch, self.yaw, self.position)
+    }
+}
 
 #[derive(Copy, Clone)]
 struct Vertex{
@@ -29,31 +58,41 @@ struct Cube<T> {
     indices: Vec<T>,
 }
 
-fn run<T: Float>(dx: T, dy: T, dz: T, x: T, y: T, z: T, count_start: T, count_step: T, near: T, far: T, field_of_view: T, aspect_ratio: T){
+fn run(debug: bool, x: f32, y: f32, z: f32, near: f32, far: f32, field_of_view: f32, d_pitch: f32, d_yaw: f32){
     let mut resolution : (u32, u32) = (1920, 1280);
-    let mut mouse_pos : (i32, i32) = (0,0);
+    let mut mouse_delta : (f32, f32);
+    let mut aspect_ratio = resolution.0 as f32 / resolution.1 as f32;
+
+    let mouse_lock_ratio : (f32, f32) = (0.5, 0.5);
+    let mut mouse_lock_position : (f32, f32) = (resolution.0 as f32 * mouse_lock_ratio.0, resolution.1 as f32 * mouse_lock_ratio.1);
 
     let facade: GlutinFacade = glium::glutin::WindowBuilder::new()
         .with_title("Smooth Voxel".to_string())
-        .with_dimensions(resolution.0, resolution.1)
+        .with_fullscreen(glium::glutin::get_primary_monitor())
         .with_depth_buffer(24)
         .build_glium()
         .unwrap();
+
+    let mut locked = true;
+
+    let window = facade.get_window().unwrap();
+
+    window.set_cursor_state(glium::glutin::CursorState::Grab).unwrap();
 
     let vertex_shader_src = r#"
         #version 140
 
         in vec3 position;
 
-        uniform mat4 camera;
-        uniform mat4 transform;
+        uniform mat4 model;
+        uniform mat4 view;
         uniform mat4 perspective;
 
         out vec3 o_pos;
 
         void main() {
             o_pos = position;
-            gl_Position = vec4(position, 1.0) * camera * transform * perspective;
+            gl_Position = vec4(position, 1.0) * model * view * perspective;
         }
     "#;
 
@@ -71,42 +110,44 @@ fn run<T: Float>(dx: T, dy: T, dz: T, x: T, y: T, z: T, count_start: T, count_st
 
     let program = glium::Program::from_source(&facade, vertex_shader_src, fragment_shader_src, None).unwrap();
 
-    let perspective = math::mat4_to_f32::<T>(
-        math::perspective_mat4_2::<T>(
-            near,
-            far,
-            field_of_view,
-            aspect_ratio
-        )
+    let mut camera: Camera = Camera::new(0.0, 0.0, math::empty_vec3());
+
+    let mut world_up = math::empty_vec3();
+    world_up[1] = 1.0;
+
+    let mut view_direction = math::empty_vec3();
+    view_direction[2] = 1.0;
+
+    let mut view = camera.view_mat4();
+
+    let mut perspective = math::perspective_mat4(
+        near,
+        far,
+        field_of_view,
+        aspect_ratio,
     );
 
-    math::print_mat4::<f32>(perspective);
-
-    let mut transform = math::mat4_to_f32::<T>(
-    math::translation_mat4::<T>([
-            x,
-            y,
-            z,
-        ])
-    );
-
-    math::print_mat4::<f32>(transform);
+    let mut model = math::translation_mat4([
+        x,
+        y,
+        z,
+    ]);
 
     let mut uniforms = uniform!{
-        camera: math::mat4_to_f32::<T>(math::identity_mat4::<T>()),
-        transform: transform,
+        model: model,
+        view: view,
         perspective: perspective,
     };
 
     let cube : Cube<u32> = Cube{vertices: [
         Vertex::new([0.0, 0.0, 0.0]),
-        Vertex::new([0.5, 0.0, 0.0]),
-        Vertex::new([0.5, 0.5, 0.0]),
-        Vertex::new([0.0, 0.5, 0.0]),
-        Vertex::new([0.0, 0.0, 0.5]),
-        Vertex::new([0.5, 0.0, 0.5]),
-        Vertex::new([0.5, 0.5, 0.5]),
-        Vertex::new([0.0, 0.5, 0.5]),
+        Vertex::new([1.0, 0.0, 0.0]),
+        Vertex::new([1.0, 1.0, 0.0]),
+        Vertex::new([0.0, 1.0, 0.0]),
+        Vertex::new([0.0, 0.0, 1.0]),
+        Vertex::new([1.0, 0.0, 1.0]),
+        Vertex::new([1.0, 1.0, 1.0]),
+        Vertex::new([0.0, 1.0, 1.0]),
         ], indices: vec!{
             0, 1, 2,
             2, 3, 0,
@@ -125,7 +166,8 @@ fn run<T: Float>(dx: T, dy: T, dz: T, x: T, y: T, z: T, count_start: T, count_st
 
             0, 1, 5,
             5, 4, 0,
-    }};
+        }
+    };
 
     let vertex_buffer = glium::VertexBuffer::new(&facade, &cube.vertices).unwrap();
 
@@ -137,6 +179,7 @@ fn run<T: Float>(dx: T, dy: T, dz: T, x: T, y: T, z: T, count_start: T, count_st
             write: true,
             .. Default::default()
         },
+        backface_culling: glium::draw_parameters::BackfaceCullingMode::CullClockwise,
         .. Default::default()
     };
 
@@ -152,43 +195,96 @@ fn run<T: Float>(dx: T, dy: T, dz: T, x: T, y: T, z: T, count_start: T, count_st
     let mut frames = 0;
     let mut ticks = 0;
 
-    let mut count : T = count_start;
-
     loop{
         let now = clock_ticks::precise_time_s();
         delta_time += now - last_time;
         last_time = now;
         while delta_time > 0.0 {
-            for ev in facade.poll_events() {
-                match ev {
-                    glium::glutin::Event::Resized(x,y) => resolution = (x, y),
-                    glium::glutin::Event::MouseMoved(coords) => mouse_pos = (coords.0, coords.1),
+            for event in facade.poll_events() {
+                match event {
+                    glium::glutin::Event::Resized(x,y) => {
+                        resolution = (x, y);
+                        mouse_lock_position = (resolution.0 as f32 / mouse_lock_ratio.0, resolution.1 as f32 / mouse_lock_ratio.1);
+                        aspect_ratio = resolution.0 as f32 / resolution.1 as f32;
+                        perspective = math::perspective_mat4(
+                            near,
+                            far,
+                            field_of_view,
+                            aspect_ratio
+                        );
+                    }
+                    glium::glutin::Event::MouseMoved(coords) => {
+                        if locked {
+                            mouse_delta = (coords.0 as f32 - mouse_lock_position.0, coords.1 as f32 - mouse_lock_position.1);
+                            camera.yaw += mouse_delta.0 as f32 / d_yaw;
+                            camera.pitch -= mouse_delta.1 as f32 / d_pitch;
+                            window.set_cursor_position(mouse_lock_position.0 as i32, mouse_lock_position.1 as i32).unwrap();
+                        }
+                    }
                     glium::glutin::Event::Closed => return,   // the window has been closed by the user
+                    glium::glutin::Event::Focused(is_focused) => {
+                        if is_focused {
+                            locked = true;
+                            window.set_cursor_state(glium::glutin::CursorState::Grab).unwrap();
+                        } else {
+                            locked = false;
+                            window.set_cursor_state(glium::glutin::CursorState::Normal).unwrap();
+                        }
+                    }
+                    glium::glutin::Event::KeyboardInput(state, id, key) => {
+                        match state {
+                            glium::glutin::ElementState::Pressed => {
+                                match key.unwrap() {
+                                    glium::glutin::VirtualKeyCode::Up => {
+
+                                    }
+                                    glium::glutin::VirtualKeyCode::Down => {
+
+                                    }
+                                    glium::glutin::VirtualKeyCode::Left => {
+
+                                    }
+                                    glium::glutin::VirtualKeyCode::Right => {
+
+                                    }
+                                    _ => ()
+                                }
+                            }
+                            glium::glutin::ElementState::Released => {
+                                match key.unwrap() {
+                                    glium::glutin::VirtualKeyCode::Up => {
+
+                                    }
+                                    glium::glutin::VirtualKeyCode::Down => {
+
+                                    }
+                                    glium::glutin::VirtualKeyCode::Left => {
+
+                                    }
+                                    glium::glutin::VirtualKeyCode::Right => {
+
+                                    }
+                                    _ => ()
+                                }
+                            }
+                        }
+                    }
                     _ => ()
                 }
             }
-            transform = math::mat4_to_f32::<T>(
-                math::translation_mat4::<T>([
-                    x,
-                    y,
-                    z,
-                ])
-            );
-            //math::print_mat4::<f32>(transform);
+            model = math::translation_mat4([
+                x,
+                y,
+                z,
+            ]);
+            view = camera.view_mat4();
             uniforms = uniform!{
-                camera: math::mat4_to_f32::<T>(
-                    math::rotation_mat4::<T>([
-                        num::cast::<i32, T>(mouse_pos.0).unwrap() / num::cast::<u32, T>(resolution.0).unwrap() * num::cast::<f64, T>(std::f64::consts::PI).unwrap(),
-                        num::cast::<i32, T>(mouse_pos.1).unwrap() / num::cast::<u32, T>(resolution.1).unwrap() * num::cast::<f64, T>(std::f64::consts::PI).unwrap(),
-                        count / dz,
-                    ])
-                ),
-                transform: transform,
+                model: model,
+                view: view,
                 perspective: perspective,
             };
             delta_time -= tps_s;
             ticks += 1;
-            count = count + count_step;
         }
         let mut frame: Frame = facade.draw();
         frame.clear_color_and_depth((0.0, 0.0, 0.0, 1.0), 1.0);
@@ -197,7 +293,9 @@ fn run<T: Float>(dx: T, dy: T, dz: T, x: T, y: T, z: T, count_start: T, count_st
         frames += 1;
 		if now > i + 1.0 {
 			i += 1.0;
-			println!("{} {}", frames.to_string(), ticks.to_string());
+            if debug {
+		        println!("{} {}", frames.to_string(), ticks.to_string());
+            }
 			frames = 0;
 			ticks = 0;
 		}
@@ -205,5 +303,14 @@ fn run<T: Float>(dx: T, dy: T, dz: T, x: T, y: T, z: T, count_start: T, count_st
 }
 
 fn main() {
-    run::<f32>(1000.0, 2000.0, 3000.0, 0.0, 0.0, -2.0, 0.0, 1.0, -10.0, 10.0, std::f32::consts::PI / 2.0, 16.0 / 9.0);
+    let debug = true;
+    let x = 0.0;
+    let y = 0.0;
+    let z = 3.0;
+    let near = -10.0;
+    let far = 10.0;
+    let field_of_view = std::f32::consts::PI / 4.0;
+    let d_pitch = 300.0;
+    let d_yaw = 300.0;
+    run(debug, x, y, z, near, far, field_of_view, d_pitch, d_yaw);
 }
--- src/math.rs
+++ src/math.rs
@@ -1,60 +1,116 @@
-extern crate num;
+#![allow(dead_code)]
+pub fn magnitude_vec3(vec3: [f32; 3]) -> f32 {
+    let mut sum : f32 = 0.0;
 
-use num::traits::{Zero, One, Float};
+    for i in 0..3 {
+        sum = sum + vec3[i].powf(2.0);
+    }
 
-pub fn mat4_to_f32<T: Float>(matrix: [[T; 4]; 4]) -> [[f32; 4]; 4] {
-    let mut temp : [[f32; 4]; 4] = [[0.0; 4]; 4];
+    sum.sqrt()
+}
 
-    for y in 0..4 {
-        for x in 0..4 {
-            temp[y][x] = matrix[y][x].to_f32().unwrap();
-        }
+pub fn scale_vec3(vec3: [f32; 3], scalar: f32) -> [f32; 3] {
+    let mut temp = empty_vec3();
+
+    for i in 0..3 {
+        temp[i] = vec3[i] * scalar;
     }
 
     temp
 }
 
-pub fn perspective_mat4_2<T: Float>(near: T, far: T, field_of_view: T, aspect_ratio: T) -> [[T; 4]; 4] {
-    let mut temp = empty_mat4::<T>();
+pub fn dot_product_vec3(vec_a: [f32; 3], vec_b: [f32; 3]) -> f32 {
+    let mut sum : f32 = 0.0;
 
-    let d: T = num::cast::<f32, T>(1.0f32).unwrap() / ((field_of_view / num::cast(2.0f32).unwrap()).tan());
+    for i in 0..3 {
+        sum = sum + vec_a[i] * vec_b[i];
+    }
+
+    sum
+}
+
+pub fn cross_product_vec3(vec_a: [f32; 3], vec_b: [f32; 3]) -> [f32; 3] {
+    let mut temp = empty_vec3();
+
+    for i in 0..3 {
+        temp[i] = vec_a[(i + 1) % 3] * vec_b[(i + 2) % 3] - vec_b[(i + 1) % 3] * vec_a[(i + 2) % 3];
+    }
+
+    temp
+}
+
+pub fn empty_vec3() -> [f32; 3] {
+    [0.0; 3]
+}
+
+pub fn view_matrix_from_radians(pitch: f32, yaw: f32, camera_position: [f32; 3]) -> [[f32; 4]; 4] {
+    let pitch_cos = pitch.cos();
+    let pitch_sin = pitch.sin();
+    let yaw_cos = yaw.cos();
+    let yaw_sin = yaw.sin();
+
+    let x = [yaw_cos, 0.0, -yaw_sin];
+    let y = [yaw_sin * pitch_sin, pitch_cos, yaw_cos * pitch_sin];
+    let z = [yaw_sin * pitch_cos, -pitch_sin, pitch_cos * yaw_cos];
+
+    let mut temp = empty_mat4();
+
+    for i in 0..3 {
+        temp[i][0] = x[i];
+        temp[i][1] = y[i];
+        temp[i][2] = z[i];
+    }
+
+    temp[0][3] = -dot_product_vec3(x, camera_position);
+    temp[1][3] = -dot_product_vec3(y, camera_position);
+    temp[2][3] = -dot_product_vec3(z, camera_position);
+    temp[3][3] = 1.0;
+
+    temp
+}
+
+pub fn view_mat4_from_vec3(view_direction: [f32; 3], world_up: [f32; 3], camera_position: [f32; 3]) -> [[f32; 4]; 4] {
+    let mut temp = empty_mat4();
+
+    let z = scale_vec3(view_direction, -magnitude_vec3(view_direction).recip());
+
+    let dx = cross_product_vec3(view_direction, world_up);
+
+    let x = scale_vec3(dx, magnitude_vec3(dx).recip());
+
+    let y = cross_product_vec3(z, x);
+
+    for i in 0..3 {
+        temp[i][0] = x[i];
+        temp[i][1] = y[i];
+        temp[i][2] = z[i];
+        temp[i][3] = camera_position[i];
+    }
+
+    temp[0][3] = -dot_product_vec3(x, camera_position);
+    temp[1][3] = -dot_product_vec3(y, camera_position);
+    temp[2][3] = -dot_product_vec3(z, camera_position);
+    temp[3][3] = 1.0;
+
+    temp
+}
+
+pub fn perspective_mat4(near: f32, far: f32, field_of_view: f32, aspect_ratio: f32) -> [[f32; 4]; 4] {
+    let mut temp = empty_mat4();
+
+    let d = 1.0 / ((field_of_view / 2.0).tan());
 
     temp[0][0] = d / aspect_ratio;
     temp[1][1] = d;
     temp[2][2] = (near + far) / (near - far);
-    temp[2][3] = (num::cast::<f32, T>(2.0f32).unwrap() * far) / (near - far);
-    temp[3][2] = num::cast(-1.0f32).unwrap();
+    temp[2][3] = (2.0 * far) / (near - far);
+    temp[3][2] = -1.0;
 
     temp
 }
 
-pub fn perspective_mat4<T: Float>(near: T, far: T, field_of_view: T, aspect_ratio: T) -> [[T; 4]; 4] {
-    let y2 = near * field_of_view.tan();
-    let y1 = -y2;
-    let x1 = y1 * aspect_ratio;
-    let x2 = y2 * aspect_ratio;
-    let temp: [[T; 4]; 4] = frustrum_mat4(x1, x2, y1, y2, near, far);
-    temp
-}
-
-pub fn frustrum_mat4<T: Float>(left: T, right: T, bottom: T, top: T, near: T, far: T) -> [[T; 4]; 4] {
-    let mut temp = empty_mat4::<T>();
-
-    temp[0][0] = (near * num::cast(2.0f32).unwrap()) / (right - left);
-    temp[1][1] = (near * num::cast(2.0f32).unwrap()) / (top - bottom);
-    temp[2][2] = (near + far) / (near - far);
-    temp[3][3] = num::cast(0.0f32).unwrap();
-
-    temp[0][2] = (right + left) / (right - left);
-    temp[1][2] = (top + bottom) / (top - bottom);
-    temp[3][2] = num::cast(-1.0f32).unwrap();
-    temp[2][3] = (far * near * num::cast(-2.0f32).unwrap()) / (far / near);
-
-    temp
-}
-
-pub fn translation_mat4<T: Float>(vector: [T; 3]) -> [[T; 4]; 4] {
-    let mut temp = identity_mat4::<T>();
+pub fn translation_mat4(vector: [f32; 3]) -> [[f32; 4]; 4] {
+    let mut temp = identity_mat4();
 
     for y in 0..3 {
         temp[y][3] = vector[y];
@@ -63,55 +119,55 @@ pub fn translation_mat4<T: Float>(vector: [T; 3]) -> [[T; 4]; 4] {
     temp
 }
 
-pub fn rotation_mat4_x<T: Float>(angle: T) -> [[T;  4]; 4] {
-    let mut temp = empty_mat4::<T>();
+pub fn rotation_mat4_x(angle: f32) -> [[f32;  4]; 4] {
+    let mut temp = empty_mat4();
 
-    temp[0][0] = One::one();
+    temp[0][0] = 1.0;
     temp[1][1] = angle.cos();
     temp[1][2] = -angle.sin();
     temp[2][1] = angle.sin();
     temp[2][2] = angle.cos();
-    temp[3][3] = One::one();
+    temp[3][3] = 1.0;
 
     temp
 }
 
-pub fn rotation_mat4_y<T: Float>(angle: T) -> [[T;  4]; 4] {
-    let mut temp = empty_mat4::<T>();
+pub fn rotation_mat4_y(angle: f32) -> [[f32;  4]; 4] {
+    let mut temp = empty_mat4();
 
     temp[0][0] = angle.cos();
     temp[0][2] = angle.sin();
-    temp[1][1] = One::one();
+    temp[1][1] = 1.0;
     temp[2][0] = -angle.sin();
     temp[2][2] = angle.cos();
-    temp[3][3] = One::one();
+    temp[3][3] = 1.0;
 
     temp
 }
 
-pub fn rotation_mat4_z<T: Float>(angle: T) -> [[T;  4]; 4] {
-    let mut temp = empty_mat4::<T>();
+pub fn rotation_mat4_z(angle: f32) -> [[f32;  4]; 4] {
+    let mut temp = empty_mat4();
 
     temp[0][0] = angle.cos();
     temp[0][1] = -angle.sin();
     temp[1][0] = angle.sin();
     temp[1][1] = angle.cos();
-    temp[2][2] = One::one();
-    temp[3][3] = One::one();
+    temp[2][2] = 1.0;
+    temp[3][3] = 1.0;
 
     temp
 }
 
-pub fn rotation_mat4<T: Float>(vector: [T; 3]) -> [[T; 4]; 4] {
-    let temp = multiply_mat4_n::<T>(vec!(rotation_mat4_x::<T>(vector[0]), rotation_mat4_y::<T>(vector[1]), rotation_mat4_z::<T>(vector[2])));
+pub fn rotation_mat4(vector: [f32; 3]) -> [[f32; 4]; 4] {
+    let temp = multiply_mat4_n(vec!(rotation_mat4_x(vector[0]), rotation_mat4_y(vector[1]), rotation_mat4_z(vector[2])));
     temp
 }
 
-pub fn multiply_mat4<T: Float>(matrix_a: [[T; 4]; 4], matrix_b: [[T; 4]; 4]) -> [[T; 4]; 4] {
-    let mut temp = empty_mat4::<T>();
+pub fn multiply_mat4(matrix_a: [[f32; 4]; 4], matrix_b: [[f32; 4]; 4]) -> [[f32; 4]; 4] {
+    let mut temp = empty_mat4();
     for x in 0..4 {
         for y in 0..4 {
-            let mut sum: T = Zero::zero();
+            let mut sum = 1.0;
             for e in 0..4 {
                 sum = sum + matrix_a[e][x] * matrix_b[y][e];
             }
@@ -121,29 +177,28 @@ pub fn multiply_mat4<T: Float>(matrix_a: [[T; 4]; 4], matrix_b: [[T; 4]; 4]) -> 
     temp
 }
 
-pub fn multiply_mat4_n<T: Float>(matrices: Vec<[[T; 4]; 4]>) -> [[T; 4]; 4] {
-    let mut temp = multiply_mat4::<T>(matrices[0], matrices[1]);
+pub fn multiply_mat4_n(matrices: Vec<[[f32; 4]; 4]>) -> [[f32; 4]; 4] {
+    let mut temp = multiply_mat4(matrices[0], matrices[1]);
     for i in 2..matrices.len() {
-        temp = multiply_mat4::<T>(temp, matrices[i]);
+        temp = multiply_mat4(temp, matrices[i]);
     }
     temp
 }
 
-pub fn empty_mat4<T: Float>() -> [[T; 4]; 4] {
-    let temp :[[T; 4]; 4] = [[Zero::zero(); 4]; 4];
-    temp
+pub fn empty_mat4() -> [[f32; 4]; 4] {
+    [[0.0; 4]; 4]
 }
 
-pub fn identity_mat4<T: Float>() -> [[T; 4]; 4] {
-    let mut temp = empty_mat4::<T>();
+pub fn identity_mat4() -> [[f32; 4]; 4] {
+    let mut temp = empty_mat4();
     for i in 0..4 {
-        temp[i][i] = One::one();
+        temp[i][i] = 1.0;
     }
     temp
 }
 
-pub fn print_mat4<T: Float>(matrix: [[T; 4]; 4]) {
+pub fn print_mat4(matrix: [[f32; 4]; 4]) {
     for y in 0..4 {
-            println!("[{}, {}, {}, {}]", matrix[y][0].to_f32().unwrap(), matrix[y][1].to_f32().unwrap(), matrix[y][2].to_f32().unwrap(), matrix[y][3].to_f32().unwrap());
+            println!("[{}, {}, {}, {}]", matrix[y][0], matrix[y][1], matrix[y][2], matrix[y][3]);
     }
 }
